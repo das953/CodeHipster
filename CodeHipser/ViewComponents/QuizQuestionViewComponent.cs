f83e912249c7e6dfd584186d02c63f865da766c7
Артем Слатвінський < >
2017-06-20 03:43:35 +0300

    Add VC for quiz questions

---
+++ CodeHipser/ViewComponents/QuizQuestionViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using CodeHipser.Models;
+
+namespace CodeHipser.ViewComponents
+{
+    public class QuizQuestionViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(
+            Question currentQuestion, 
+            int currentCount, 
+            int totalCount,
+            string themeName)
+        {
+            ViewData["currentCount"] = currentCount;
+            ViewData["totalCount"] = totalCount;
+            ViewData["themeName"] = themeName;
+            return View(currentQuestion);
+           
+        }
+    }
+}
